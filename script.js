765ad9b1c3a735f7775ce41fe8a0c1dcdc5dabb7
Matthew <mmgligore@gmail.com>
2024-12-10 21:57:56 -0500

    basic_setup

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const terms = ["Full-Stack Developer", "UI/UX Designer", "Product Manager", "Game Developer"];
+    let termIndex = 0;
+    let charIndex = 0;
+    const span = document.querySelector(".typed-text");
+
+    function type() {
+        if (charIndex < terms[termIndex].length) {
+            span.textContent = span.textContent.replace('_', terms[termIndex].charAt(charIndex-1));  // Remove the extra space if present
+            span.textContent += terms[termIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 100);
+        } else {
+            setTimeout(erase, 2000);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 1) {
+            span.textContent = span.textContent.slice(0, -1);
+            charIndex--;
+            setTimeout(erase, 100);
+        } else {
+            span.textContent = '_';  // Add an extra space when text is empty
+            termIndex = (termIndex + 1) % terms.length;
+            setTimeout(type, 1000);
+        }
+    }
+
+    if (terms.length) type();
+});
