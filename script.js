3aef11926f620fd7aa227387ea1c7c59ae13adec
Matthew <mmgligore@gmail.com>
2024-12-12 15:58:38 +0000

    Feedback Form animation

--- script.js
+++ script.js
@@ -68,3 +68,79 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const feedbackButton = document.getElementById('feedback-button');
+    const feedbackContainer = document.getElementById('feedback-container');
+
+    feedbackButton.addEventListener('click', function () {
+        feedbackContainer.classList.add('visible');
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!feedbackContainer.contains(event.target) && event.target !== feedbackButton) {
+            feedbackContainer.classList.remove('visible');
+        }
+    });
+
+    document.getElementById("form").addEventListener("submit", function (e) {
+        e.preventDefault(); // Prevent the default form submission
+        document.getElementById("message").textContent = "Submitting..";
+        document.getElementById("message").style.display = "block";
+        document.getElementById("submit-button").disabled = true;
+
+        // Collect the form data
+        var formData = new FormData(this);
+        var keyValuePairs = [];
+        for (var pair of formData.entries()) {
+            keyValuePairs.push(pair[0] + "=" + pair[1]);
+        }
+
+        var formDataString = keyValuePairs.join("&");
+
+        // Send a POST request to your Google Apps Script
+        fetch(
+            "https://script.google.com/macros/s/AKfycbyNxDZE-OrTP7l6jyBAkNiF4T7qS7b9ip6eO7xV8NDvAYEnnxVNH0k-7YiQVB2Sr78m/exec",
+            {
+                redirect: "follow",
+                method: "POST",
+                body: formDataString,
+                headers: {
+                    "Content-Type": "text/plain;charset=utf-8",
+                },
+            }
+        )
+            .then(function (response) {
+                // Check if the request was successful
+                if (response) {
+                    return response; // Assuming your script returns JSON response
+                } else {
+                    throw new Error("Failed to submit the form.");
+                }
+            })
+            .then(function (data) {
+                // Display a success message
+                document.getElementById("message").textContent =
+                    "Data submitted successfully!";
+                document.getElementById("message").style.display = "block";
+                document.getElementById("message").style.backgroundColor = "green";
+                document.getElementById("message").style.color = "beige";
+                document.getElementById("submit-button").disabled = false;
+                document.getElementById("form").reset();
+
+                setTimeout(function () {
+                    document.getElementById("message").textContent = "";
+                    document.getElementById("message").style.display = "none";
+                }, 2600);
+            })
+            .catch(function (error) {
+                // Handle errors, you can display an error message here
+                console.error(error);
+                document.getElementById("message").textContent =
+                    "An error occurred while submitting the form.";
+                document.getElementById("message").style.display = "block";
+            });
+    });
+});
+
+
